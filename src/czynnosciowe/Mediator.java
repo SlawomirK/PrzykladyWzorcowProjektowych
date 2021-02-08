d297c0a40ce9ca209baad6d1e994aac7fab02099
Slawek <Kobylinski.s@gmail.com>
2021-02-08 17:24:48 +0100

    mediator

---
+++ src/czynnosciowe/Mediator.java
@@ -0,0 +1,42 @@
+package czynnosciowe;
+
+interface ChatMediator {
+    void sendMessage(User user, String message);
+}
+
+public class Mediator {
+    public static void main(String[] args) {
+        var mediator = new ChatMediatorClass();
+        var pawel = new User("Pawel", mediator);
+        var tomek = new User("Tomek", mediator);
+        pawel.send("czesc tomek");
+        tomek.send("czesc Pawel");
+    }
+}
+
+class ChatMediatorClass implements ChatMediator {
+
+    @Override
+    public void sendMessage(User user, String message) {
+        String sender = user.getName();
+        System.out.println("Nadawca: " + sender + "wiadomość: " + message);
+    }
+}
+
+class User {
+    private final String name;
+    private final ChatMediator chatMediator;
+
+    public User(String name, ChatMediator chatMediator) {
+        this.name = name;
+        this.chatMediator = chatMediator;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void send(String message) {
+        this.chatMediator.sendMessage(this, message);
+    }
+}
